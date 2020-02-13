21372a2790c9c9e4c927fac8a241f1943a538f6f
bossm0n5t3r <bossm0n5t3r@gmail.com>
2020-02-13 22:58:54 +0900

    Add keilo.hpp

---
+++ src/keilo.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <algorithm>
+#include <fstream>
+#include <list>
+#include <map>
+#include <mutex>
+#include <string>
+#ifdef _WIN32
+#include <filesystem>
+#elif __linux__ || __APPLE__
+#include <experimental/filesystem>
+#endif
+#include <sstream>
+
+namespace keilo {
+enum class result_t {
+  success = 0,
+  fail,
+  has_no_value,
+  cannot_find,
+  already_exist,
+  key_overlapped,
+  key_not_exist,
+  file_not_support
+};
+}
